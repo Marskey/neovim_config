fcc755341c667d3df025fc612d6f6cef40bb7d73
Marskey <marskeymill@icloud.com>
2022-07-17 22:41:47 +0800

    update

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -10,7 +10,19 @@ telescope.setup {
 
     --prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart" },
+   -- "hidden"    hide file names
+   -- "tail"      only display the file name, and not the path
+   -- "absolute"  display absolute paths
+   -- "smart"     remove as much from the path as possible to only show
+   --             the difference between the displayed paths.
+   --             Warning: The nature of the algorithm might have a negative
+   --             performance impact!
+   -- "shorten"   only display the first character of each directory in
+   --             the path
+   -- "truncate"  truncates the start of the path when the whole path will
+   --              not fit. To increase the the gap between the path and the edge.
+   --              set truncate to number `truncate = 3`
+    path_display = { shorten = { len = 3, exclude = {1, 2, -2, -1} } },
 
     mappings = {
       i = {
