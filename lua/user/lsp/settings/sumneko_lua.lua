f991a9f51a09abda0fb278d67d57c8be14500852
Marskey <marskeymill@icloud.com>
2022-07-20 10:44:45 +0800

    update

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -1,26 +1,26 @@
 return {
-	settings = {
-		Lua = {
-			diagnostics = {
-        enable = true,
-        globals = {"vim"},
-        ignoredFiles = "Opened",
-        neededFileStatus = "Opened"
-        --workspaceDelay = 6000,
-			},
-			workspace = {
-        useGitIgnore = true,
-        maxPreload = 5000,
-        checkThirdParty = true
-			},
-      runtime = {
-        version = 'lua 5.3',
-        special = {
-          include = "require",
-          gSingleton = "require"
+    settings = {
+        Lua = {
+            diagnostics = {
+                enable = true,
+                globals = {"vim"},
+                ignoredFiles = "Opened",
+                neededFileStatus = "Opened",
+                workspaceDelay = 6000,
+            },
+            workspace = {
+                useGitIgnore = true,
+                -- maxPreload = 5000,
+                checkThirdParty = true
+            },
+            runtime = {
+                version = 'lua 5.3',
+                special = {
+                    include = "require",
+                    gSingleton = "require"
+                },
+                pathStrict = false
+            },
         },
-        pathStrict = false
-      },
-		},
-	},
+    },
 }
