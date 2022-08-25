cc8c264e4e963fae917b4a81acf547888d3fa05b
Marskey <marskeymill@icloud.com>
2022-08-25 18:27:05 +0800

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,7 +59,7 @@ return packer.startup(function(use)
     use({ "akinsho/toggleterm.nvim" })
     use({ "ahmedkhalf/project.nvim" })
     use({ "lewis6991/impatient.nvim" })
-    use({ "lukas-reineke/indent-blankline.nvim" })
+    -- use({ "lukas-reineke/indent-blankline.nvim" })
     use({ "goolord/alpha-nvim" })
     use( "folke/which-key.nvim" )
 
