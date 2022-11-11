82ab632a41dbf901d3c4d162eb06bcf09c385c52
Marskey <marskeymill@icloud.com>
2022-11-11 15:55:12 +0800

    update to nvim 0.8

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -2,150 +2,113 @@ local M = {}
 
 -- TODO: backfill this to template
 M.setup = function()
-	-- local signs = {
-	-- 	{ name = "DiagnosticSignError", text = " " },
-	-- 	{ name = "DiagnosticSignWarn", text = " " },
-	-- 	{ name = "DiagnosticSignHint", text = " " },
-	-- 	{ name = "DiagnosticSignInfo", text = " " },
-	-- }
-	--
-	-- for _, sign in ipairs(signs) do
-	-- 	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	-- end
+  -- local signs = {
+  -- 	{ name = "DiagnosticSignError", text = " " },
+  -- 	{ name = "DiagnosticSignWarn", text = " " },
+  -- 	{ name = "DiagnosticSignHint", text = " " },
+  -- 	{ name = "DiagnosticSignInfo", text = " " },
+  -- }
+  --
+  -- for _, sign in ipairs(signs) do
+  -- 	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  -- end
 
-	local config = {
-		-- disable virtual text
-		virtual_text = false,
-		-- show signs
-		-- signs = {
-		-- 	active = signs,
-		-- },
-		update_in_insert = true,
-		underline = true,
-		severity_sort = true,
-		float = {
-			focusable = false,
-			style = "minimal",
-			border = "rounded",
-			source = "always",
-			header = "",
-			prefix = "",
-		},
-	}
+  local config = {
+    -- disable virtual text
+    virtual_text = false,
+    -- show signs
+    -- signs = {
+    -- 	active = signs,
+    -- },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
 
-	vim.diagnostic.config(config)
+  vim.diagnostic.config(config)
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded"
-	})
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded"
+  })
 
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded"
-	})
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded"
+  })
 
-    local location_handler = function (err, result, ctx, config)
-        if result == nil or vim.tbl_isempty(result) then
-            local posParam = vim.lsp.util.make_position_params()
-            if posParam.textDocument.uri == ctx.params.textDocument.uri
-                and posParam.position.character == ctx.params.position.character
-                and posParam.position.line == ctx.params.position.line then
-                vim.lsp.util.open_floating_preview({"No location found!"}, 'markdown', {border = "rounded", focus = false})
-            end
-            return nil
-        end
-
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-
-        config = config or {}
-
-        if vim.tbl_islist(result) then
-            local title = 'LSP locations'
-            local items = vim.lsp.util.locations_to_items(result, client.offset_encoding)
-
-            if config.on_list then
-                assert(type(config.on_list) == 'function', 'on_list is not a function')
-                config.on_list({ title = title, items = items })
-            else
-                if #result == 1 then
-                    vim.lsp.util.jump_to_location(result[1], client.offset_encoding, config.reuse_win)
-                    return
-                end
-                vim.fn.setqflist({}, ' ', { title = title, items = items })
-                vim.api.nvim_command('botright copen')
-            end
-        else
-            vim.lsp.util.jump_to_location(result, client.offset_encoding, config.reuse_win)
-        end
+  local location_handler = vim.lsp.handlers["textDocument/definition"]
+  vim.lsp.handlers["textDocument/definition"] = function(err, result, ctx, config)
+    if result == nil or vim.tbl_isempty(result) then
+      local posParam = vim.lsp.util.make_position_params()
+      if posParam.textDocument.uri == ctx.params.textDocument.uri
+          and posParam.position.character == ctx.params.position.character
+          and posParam.position.line == ctx.params.position.line then
+        vim.lsp.util.open_floating_preview({ "No location found!" }, 'markdown', { border = "rounded", focus = false })
+      end
+      return nil
     end
 
-    vim.lsp.handlers["textDocument/definition"] = location_handler
-  --   vim.lsp.handlers["textDocument/definition"] = function (err, result, ctx, config)
-  --   if result == nil or vim.tbl_isempty(result) then
-  --     local posParam = vim.lsp.util.make_position_params()
-  --     if posParam.textDocument.uri == ctx.params.textDocument.uri
-  --       and posParam.position.character == ctx.params.position.character
-  --       and posParam.position.line == ctx.params.position.line then
-  --       vim.lsp.util.open_floating_preview({"No location found!"}, 'markdown', {border = "rounded", focus = false})
-  --     end
-  --     return nil
-  --   end
-  --
-  --   location_handler(err, result, ctx, config)
-  -- end
+    location_handler(err, result, ctx, config)
+  end
 end
 
 local function lsp_highlight_document(client)
-	-- Set autocommands conditional on server_capabilities
-	local status_ok, illuminate = pcall(require, "illuminate")
-	if not status_ok then
+  -- Set autocommands conditional on server_capabilities
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if not status_ok then
     vim.notify('illuminate not found!')
-		return
-	end
-	illuminate.on_attach(client)
-	-- end
+    return
+  end
+  illuminate.on_attach(client)
+  -- end
 end
 
 local function lsp_keymaps(bufnr)
-	local opts = { noremap = true, silent = true }
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(
-		bufnr,
-		"n",
-		"gl",
-		'<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>',
-		opts
-	)
-	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "gl",
+    '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>',
+    opts
+  )
+  vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
 end
 
 M.on_attach = function(client, bufnr)
-	-- vim.notify(client.name .. " starting...")
-	-- TODO: refactor this into a method that checks if string in list
-	-- if client.name == "tsserver" then
-	-- 	client.resolved_capabilities.document_formatting = false
-	-- end
-	lsp_keymaps(bufnr)
-	-- lsp_highlight_document(client)
+  -- vim.notify(client.name .. " starting...")
+  -- TODO: refactor this into a method that checks if string in list
+  -- if client.name == "tsserver" then
+  -- 	client.resolved_capabilities.document_formatting = false
+  -- end
+  lsp_keymaps(bufnr)
+  -- lsp_highlight_document(client)
 
   require("aerial").on_attach(client, bufnr)
   require("lsp_signature").on_attach()
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
-	return
+  return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
